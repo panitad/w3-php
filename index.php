8d788dbbc8da06e168483310b290e922b1ccda85
panitad <139569094+panitad@users.noreply.github.com>
2024-07-10 09:07:18 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panitad24</title>
+</head>
+<body>
+    <?php
+        $a=3;
+        $b=4;
+        echo "<h3>ผลลัพธ์</h3>";
+        echo "ค่าของ \$a = 3"."<br> ค่าของ \$b = 4 <br> ผลรวมของ \$a และ \$b คือ " . $a+$b;
+        echo "<br>";
+        $A=20.5;
+        $B=20.5;
+        $C=$A==$B;
+        echo "<h3>ผลลัพธ์</h3>";
+        echo "\$A=B"."<br> \$A=20.5"."<br> \$B=20.5"."<br> คำตอบของ \$A=\$B คือ " . $C;
+    ?>
+</body>
+</html>
